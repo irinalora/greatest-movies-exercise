23d5430105e711e398429e56a19cd93a78de4268
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE.local>
2022-03-11 13:57:05 +0100

    WP

--- src/movies.js
+++ src/movies.js
@@ -5,43 +5,68 @@
 // Retorna un array amb tots els directors.
 // BONUS: Sembla que molts directors han fet diverses pel·lícules. Com podríem "netejar" aquest
 // array per eliminar els duplicats?
-function getAllDirectors(movies) {}
+function getAllDirectors(movies) {
+  return movies.map((movie) => movie.director);
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 // --- CATALÀ ---
 // Steven Spielberg. És el millor? - Quantes pelis de drama ha dirigit Steven Spielberg?
-function howManyMovies(movies) {}
+// function howManyMovies(movies) {
+//   return movies.filter((movie) => {
+//     return (
+//       movie.genre.includes('Drama') && movie.director === 'Steven Spielberg'
+//     );
+//   }).length; // para convertirlo en numero
+// }
+
+
+// // function howManyMovies(someMovies) {
+//   return someMovies.filter(
+//     eachMovie =>
+//       eachMovie.director === 'Steven Spielberg' &&
+//       eachMovie.genre.includes('Drama')
+//   ).length;
+// } // tot un lenght per convertir drama en numeros
+
+// 
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 // --- CATALÀ ---
 // La mitjana de puntuació. Extreu la mitjana de totes les puntuacions de les pel·lícules,
 // amb dos decimals.
-function scoresAverage(movies) {}
+function scoresAverage(movies) {
+  const sumaDetot = movies.reduce((acc, movie) => {
+    return movie.score + acc;
+  }, 0)
+  const reuslt = (sumaDeTot / movies.length).toFixed(2);
+  return Number(resultat);
 
+}
 // Iteration 4: Drama movies - Get the average of Drama Movies
 // --- CATALÀ ---
 // Pel·lícules de drama. Extreu la mitjana de les Películes de drama.
-function dramaMoviesScore(movies) {}
+function dramaMoviesScore(movies) { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 // --- CATALÀ ---
 // Ordenar per any. Order per ordre ascendent (creixent) les pel·lícules.
-function orderByYear(movies) {}
+function orderByYear(movies) { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 // --- CATALÀ ---
 // Ordre alfabètic - Ordena per títol i extreu un array amb només els 20 primers títols.
-function orderAlphabetically(movies) {}
+function orderAlphabetically(movies) { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 // --- CATALÀ ---
 // BONUS: Format de temps. Canvia la duració de les pel·lícules de hores a minuts.
-function turnHoursToMinutes(movies) {}
+function turnHoursToMinutes(movies) { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 // --- CATALÀ ---
 // BONUS: Millor mitjana per any.
-function bestYearAvg(movies) {}
+function bestYearAvg(movies) { }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
